7d00b6a02762f8c326699517b219549950c74bbf
cjsdiniz <cjsdiniz@outlook.com>
2022-02-23 18:34:22 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//// Functions
+//Function example 1
 function logger() {
     console.log('Log entry')
 }
@@ -8,3 +10,49 @@ function logger() {
 logger();
 logger();
 logger();
+
+//Function example 2
+function fruitProcessor(apples, oranges) {
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and
+    ${oranges} oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(5,2));
+
+
+//Function example 3 - Declaration
+function calcAge1(birthYear) {
+    return new Date().getFullYear() - birthYear;
+    
+}
+console.log(calcAge1(50));
+
+//Function example 4 - Expression
+const calcAge2 = function (birthYear) {
+    return new Date().getFullYear() - birthYear;
+}
+console.log(calcAge2(33));
+
+//Function example 5 - Arrow Function 1
+const calcAge3 = (birthYear) => {
+    return new Date().getFullYear() - birthYear;
+    
+}
+console.log(calcAge3(13));
+
+//Function example 5 - Arrow Function 2
+const calcAge4 = birthYear => new Date().getFullYear() - birthYear;
+console.log(calcAge4(23));
+
+//Functios Years until retirement
+const yearsUntilRetire = birthYear => {
+    const age = 2022 - birthYear;
+    const retirement = 68 - age;
+    return retirement;
+}
+
+console.log(yearsUntilRetire(1968));
+
+
