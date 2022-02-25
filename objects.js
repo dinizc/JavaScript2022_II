32cd3ad6174c75412c5482bcc948383ec7e1149f
cjsdiniz <cjsdiniz@outlook.com>
2022-02-25 20:53:23 +0000

    Objects

---
+++ objects.js
@@ -0,0 +1,23 @@
+// Arrau exemple
+const personArray = [
+    'John',
+    'Read',
+    2022-1990,
+    'programmer',
+    ['Michael', 'John', 'Steven']
+]
+
+// Objects
+const person = {
+    firstName: 'John',
+    lastName: 'Read',
+    age: 2022-1990,
+    job: 'programmer',
+    friends: ['Michael', 'John', 'Steven']
+}
+console.log(person);
+console.log(`Age: ${person.age}`);
+console.log(`Friends: ${person['friends']}`);
+
+const nameKey = 'Name';
+console.log(person['first'+nameKey], person['last'+nameKey]);
