02ee79fe30f9a7da7b8a07c3fbaafb9486e1e1d9
cjsdiniz <cjsdiniz@outlook.com>
2022-03-15 10:32:53 +0000

    Dot vs Bracket

--- objects.js
+++ objects.js
@@ -17,7 +17,20 @@ const person = {
 }
 console.log(person);
 console.log(`Age: ${person.age}`);
-console.log(`Friends: ${person['friends']}`);
+console.log(`Friends by bracks notation: ${person['friends']}`);
+console.log(`Friends by dot notation: ${person.friends}`);
 
 const nameKey = 'Name';
 console.log(person['first'+nameKey], person['last'+nameKey]);
+
+const prop = prompt("Choose John's property? ");
+console.log(prop);
+console.log(person[prop]);
+
+console.log('***********************');
+
+//Chalenge:
+//John as 3 friends and his best friend is called Michael
+console.log(`${person.firstName} as ${person.friends.length} \
+    friends and his best friend is called ${person.friends[0]}`);
+        
