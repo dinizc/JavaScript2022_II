4590f9679ee92a74b9cdb7c7388ebccc95cc9b89
cjsdiniz <cjsdiniz@outlook.com>
2022-03-16 10:32:05 +0000

    Iterations-1

---
+++ iterations.js
@@ -0,0 +1,18 @@
+// Iterations / loops
+// Example 1
+for (let i = 1; i < 4; i++) {
+    console.log(`Lifting weights rep ${i} 🏋️`);
+}
+
+// Example 2
+const personArray = [
+    'John',
+    'Read',
+    2022-1990,
+    'programmer',
+    ['Michael', 'John', 'Steven']
+]
+
+for (let i = 0; i < personArray.length; i++) {
+    
+}
